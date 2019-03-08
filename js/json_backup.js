7785acda0231d5e1077409fee12d554229b29af5
yongaro <resodafk@gmail.com>
2019-03-08 21:43:00 +0100

    Security and broken image fix

--- js/json_backup.js
+++ js/json_backup.js
@@ -55,7 +55,7 @@ const static_project_json_string = '{\
     },\
     {\
       "content" :[\
-        {"type" : "Image", "file" : "SeriousFarm1.jpg"}, {"type" : "Image", "file" : "spring.png"}, {"type" : "Image", "file" : "automn.png"}, {"type" : "Image", "file" : "winter.png"}\
+        {"type" : "Image", "file" : "SeriousFarm.jpg"}, {"type" : "Image", "file" : "spring.png"}, {"type" : "Image", "file" : "automn.png"}, {"type" : "Image", "file" : "winter.png"}\
       ]\
     },\
     {\
